6a4d71ba80fa04dfdeb465455be7ea8456ca3f73
JaelSantos01 <20223TN110@utez.edu.mx>
2024-11-13 20:15:59 -0600

    Primer subida a front

--- SIFER-Front/src/auth/SinginPage.jsx
+++ SIFER-Front/src/auth/SinginPage.jsx
@@ -1,35 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Perfil from '../assets/usuario.png'
+import Perfil from '../assets/img/perfil.png'
+import Employees from '../assets/img/employees.png'
+import Historial from '../assets/img/historial.png'
+import Inventario from '../assets/img/inven.png'
+import { useState } from 'react';
+import Logo from '../assets/img/logo.png'
+import Letras from '../assets/img/nombre.png'
 
 const blue = "#282C37";
+const orange = '#F75409';
+
 export const SinginPage = () => {
+
+    const [hovered1, setHovered1] = useState(false);
+    const [hovered2, setHovered2] = useState(false);
+    const [hovered3, setHovered3] = useState(false);
+    const [hovered4, setHovered4] = useState(false);
+
     return(
         <>
             <nav class="navbar navbar-expand-lg p-0 ">
                 <div style={{backgroundColor: blue}} class="container-fluid">
-                    <a class="navbar-brand text-white" href="#"> Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    </button>
+                    <a class="navbar-brand text-white" href="#"> 
+                        <img src={Letras} style={{width: 250, height: 50}} />
+                    </a>
+                    
                     <div class="collapse navbar-collapse p-4" id="navbarSupportedContent">
+                        <a class= "nav-link active text-white fs-4" href="#">  Herramientas </a>
+
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                        <div class="bg-white rounded-pill border border-dark-subtle d-flex position-absolute start-50 translate-middle">
-                            <li class="nav-item">
-                            <a class="nav-link active px-4" aria-current="page" href="#"> <img src={Perfil} style={{width: 30, height: 30}} alt="usuarios"/></a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link active px-4" aria-current="page" href="#"> <img src={Perfil} style={{width: 30, height: 30}} alt="usuarios"/></a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link active px-4" aria-current="page" href="#"> <img src={Perfil} style={{width: 30, height: 30}} alt="usuarios"/></a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link active px-4" aria-current="page" href="#"> <img src={Perfil} style={{width: 30, height: 30}} alt="usuarios"/></a>
-                            </li>
+                        <div class="d-flex position-absolute start-50 translate-middle">
+                            <input style={{width: 450}} class="form-control me-2 rounded-pill border border-dark-subtle" type="search" placeholder="Search" aria-label="Search"/>
+                            <img src="" alt="" />
                         </div>
                         
                     </ul>
-                        <button class="rounded-pill ">Salir</button>
+                        <button class=" btn rounded-pill fs-5" style={{backgroundColor: orange, width: 100, height: 37}}>Salir</button>
                     </div>
                 </div>
             </nav>
