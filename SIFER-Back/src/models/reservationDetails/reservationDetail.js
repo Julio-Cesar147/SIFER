0d400e5f23cb00822926a5fb3179e2f64e360db6
Julio Cesar <20203tn160@utez.edu.mx>
2024-11-19 17:56:12 -0600

    [ADD] reservationDetail table model

---
+++ SIFER-Back/src/models/reservationDetails/reservationDetail.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const sequelize = require('../../config/database')
+
+const ReservationDetail = sequelize.define( 'ReservationDetail', {
+    idReDetails: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    reserved_quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    reserved: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    product: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},{
+    tablename: 'reservation_details',
+    timestamps: false
+})
+
+module.exports = {
+    ReservationDetail
+}
