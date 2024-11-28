6e588779bf0e8d43abf20e970f53b34e7f1a31e4
Julio Cesar <20203tn160@utez.edu.mx>
2024-11-28 10:53:03 -0600

    [ADD] configuration for sending mail

--- SIFER-Back/src/utils/functions.js
+++ SIFER-Back/src/utils/functions.js
@@ -1,4 +1,6 @@
+process.loadEnvFile()
 const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
 
 // Funcion para hashear la contrasenia
 const hashPayload = async (payload) => {
@@ -11,7 +13,25 @@ const comparePayload = async (payload, hashedPayload) => {
     return await bcrypt.compare(payload, hashedPayload)
 }
 
+// Funcion que configura el envio de correo
+const configSendEmail = async (mailOptions) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EHOST,
+        port: process.env.EPORT,
+        secure: false,
+        auth: {
+            user: process.env.EUSER,
+            pass: process.env.EPASS
+        },
+        connectionTimeout: 6000,
+        socketTimeout: 6000
+    })
+
+    return await transporter.sendMail(mailOptions)
+}
+
 module.exports = {
     hashPayload,
-    comparePayload
+    comparePayload,
+    configSendEmail
 }
