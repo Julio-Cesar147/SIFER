11bab1d2084156649650a7c50feec80ec85aaa49
Julio Cesar <20203tn160@utez.edu.mx>
2024-11-29 17:50:37 -0600

    [UPDATE] connection of the register for user

--- SIFER-Front/src/auth/Register.jsx
+++ SIFER-Front/src/auth/Register.jsx
@@ -1,27 +1,69 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Logo from '../assets/img/logo.png'
 import Letras from '../assets/img/nombre.png'
 import Lupa from '../assets/img/buscar.png'
 import { Button, FloatingLabel, Label } from 'flowbite-react';
+import apiConnect from '../utils/api.connection';
 const blue = "#282C37";
 const orange = '#F75409';
 
 const Register = () =>{
 
-    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
+    const [lastnames, setLastnames] = useState('')
+    const [email, setEmail] = useState('')
+    const [birthday, setBirthday] = useState('')
+    const [telephone, setTelephone] = useState('')
+    const [occupations, setOccupations] = useState([])
+    const [occupation, setOccupation] = useState('')
+    const [street, setStreet] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [postal, setPostal] = useState('')
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
 
+    useEffect(() => {
+        getAllOccupations()
+    }, [])
     
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        try {
+            const lastname = lastnames.split(' ')[0]
+            const surname = lastnames.split(' ')[1]
+            const role = 3
+
+            const payload = { name, lastname, surname, email, password, telephone, birthday, role, occupation, street, city, state, postal }
+
+            const result = await apiConnect.post('api/auth/register', payload)
+
+            if (result) {
+                console.log('Registro exitoso');
+            } else {
+                console.error('Error en el registro');
+            }
+        } catch (error) {
+            console.error(error)
+        }
         // Funcionalidad de autenticación
     };
+
+    const getAllOccupations = async () => {
+        try {
+            const response = await apiConnect.get('api/occupation/')
+
+            setOccupations(response.occupations)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     const [searchTerm, setSearchTerm] = useState('');
 
@@ -59,36 +101,27 @@ const Register = () =>{
                 
                 <div className="d-flex flex-column justify-content-center align-items-center p-4" style={{ width: '50%' }}>
                     <p className="fw-medium fs-4 mb-4">Registrarse</p>
-                        <form className="w-75">
+                        <form className="w-75" onSubmit={handleSubmit}>
                             <div className="mb-3">
                                 <Label htmlFor="username" className="form-label fw-bold text-primary">Nombre:</Label>
-                                    <input type="text" className="form-control" id="username" name="username" placeholder="Usuario" required/>
+                                    <input type="text" className="form-control" id="username" name="username" placeholder="Usuario" required value={name} onChange={(e) => setName(e.target.value)}/>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="lastnames" className="form-label fw-bold text-primary">Apellidos:</label>
-                                <input type= "text" className="form-control" id="apellidos" name="lastnames" placeholder='Apellidos' required/>
-                            </div>
-                            
-                            <div className="d-flex mb-3">
-                                <div className="col-6">
-                                    <label htmlFor="date" className='form-label fw-bold text-primary'>Fecha de Nacimiento:</label>
-                                    <input type="text" className='form-control' id='date' name='date' placeholder='Fecha de nacimiento' required/>
-                                </div>
-                                <div className="col-6 ms-2">
-                                    <label htmlFor="phone" className='form-label fw-bold text-primary'>Teléfono:</label>
-                                    <input type="text" className='form-control' id='phone' name='phone' placeholder='Núemero telefonico' required/>
-                                </div>
+                                <input type= "text" className="form-control" id="lastnames" name="lastnames" placeholder='Apellidos' required value={lastnames} onChange={(e) => setLastnames(e.target.value)}/>
                             </div>
 
                             <div className="mb-3">
                                 <label htmlFor="email" className='form-label fw-bold text-primary'>Correo Electrónico:</label>
-                                <input type="text" className='form-control' id='email' name='phone' placeholder='Correo electrónico' required/>
+                                <input type="text" className='form-control' id='email' name='email' placeholder='Correo electrónico' required value={email} onChange={(e) => setEmail(e.target.value)}/>
                             </div>
 
-                            <div className="d-flex mb-3">
-                            <input type={showPassword ? "text" : "password"} className="form-control" name="password" placeholder='********'/>
-                            <button type="button" className="btn btn-outline-secondary border-white" onClick={() => setShowPassword(!showPassword)}>
+                            
+                            <label htmlFor="email" className='form-label fw-bold text-primary'>Contraseña:</label>
+                            <div className="mb-3 d-flex">
+                            <input type={showPassword ? "text" : "password"} className="form-control" name="password" placeholder='********' required value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            <button type="button" className="btn btn-outline-secondary border-white " onClick={() => setShowPassword(!showPassword)}>
                                 <img src={ showPassword
                                         ? "https://cdn-icons-png.flaticon.com/128/565/565655.png" 
                                         : "https://cdn-icons-png.flaticon.com/128/2874/2874780.png" 
@@ -97,6 +130,53 @@ const Register = () =>{
                                 style={{ width: "20px", height: "20px" }}/>
                             </button>
                         </div>
+                            
+                            <div className="d-flex mb-3">
+                                <div className="col-6">
+                                    <label htmlFor="date" className='form-label fw-bold text-primary'>Fecha de Nacimiento:</label>
+                                    <input type="text" className='form-control' id='date' name='date' placeholder='Fecha de nacimiento' required value={birthday} onChange={(e) => setBirthday(e.target.value)}/>
+                                </div>
+                                <div className="col-6 ms-2">
+                                    <label htmlFor="phone" className='form-label fw-bold text-primary'>Teléfono:</label>
+                                    <input type="text" className='form-control' id='phone' name='phone' placeholder='Número telefonico' required value={telephone} onChange={(e) => setTelephone(e.target.value)}/>
+                                </div>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="lastnames" className="form-label fw-bold text-primary">Ocupacion:</label>
+                                <select className="form-control" id="occupation" name="occupation" required value={occupation} onChange={(e) => setOccupation(e.target.value)}>
+                                <option value="" disabled>Selecciona una ocupación</option>
+                                    {occupations.map((occupation) => (
+                                        <option key={occupation.idOccupation} value={occupation.idOccupation}>
+                                        {occupation.occupation}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+
+                            <div className="d-flex mb-3">
+                                <div className="col-6">
+                                    <label htmlFor="street" className='form-label fw-bold text-primary'>Direccion:</label>
+                                    <input type="text" className='form-control' id='street' name='street' placeholder='Direccion' required value={street} onChange={(e) => setStreet(e.target.value)}/>
+                                </div>
+                                <div className="col-6 ms-2">
+                                    <label htmlFor="city" className='form-label fw-bold text-primary'>Ciudad:</label>
+                                    <input type="text" className='form-control' id='city' name='city' placeholder='Ciudad' required value={city} onChange={(e) => setCity(e.target.value)}/>
+                                </div>
+                            </div>
+
+                            <div className="d-flex mb-3">
+                                <div className="col-6">
+                                    <label htmlFor="state" className='form-label fw-bold text-primary'>Estado:</label>
+                                    <input type="text" className='form-control' id='state' name='state' placeholder='Estado' required value={state} onChange={(e) => setState(e.target.value)}/>
+                                </div>
+                                <div className="col-6 ms-2">
+                                    <label htmlFor="postal" className='form-label fw-bold text-primary'>Codigo Postal:</label>
+                                    <input type="text" className='form-control' id='postal' name='postal' placeholder='Codigo Postal' required value={postal} onChange={(e) => setPostal(e.target.value)}/>
+                                </div>
+                            </div>
+
+                            
 
                             <button type="submit" className="btn btn-primary w-100 mb-4"> Registrarse </button>
                         </form>
